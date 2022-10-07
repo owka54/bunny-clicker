892a63f9ad7a563a50166002bbeb67c3dbea6698
Owen <owen.kane54@gmail.com>
2022-10-07 09:13:03 +0100

    Add rose item & improve styling

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,4 +1,5 @@
-let carrots = 0;
+let carrots = 500;
+let allTimeCarrots = 500;
 let perClick = 1;
 let perSecond = 0;
 
@@ -8,12 +9,16 @@ let pawCost = 10;
 let hayTotal = 0;
 let hayCost = 100;
 
+let flowerTotal = 0;
+let flowerCost = 500;
+
 let bunnyTotal = 0;
-let bunnyCost = 500;
+let bunnyCost = 1000;
 
 
 function carrotClick() {
     carrots += perClick;
+    allTimeCarrots += perClick
     document.getElementById('total-carrots').innerHTML = Math.floor(carrots);
 }
 
@@ -22,11 +27,12 @@ var interval = setInterval(idleIncrement, 1000);
 // Increment carrot count by idle amount
 function idleIncrement() {
     carrots += perSecond;
+    allTimeCarrots += perSecond
     document.getElementById('total-carrots').innerHTML = Math.floor(carrots);
 }
 // Calculate and display carrots per second
 function idleCarrots() {
-    perSecond = ((0.1 * pawTotal) + (10 * bunnyTotal) + (1 * hayTotal))
+    perSecond = ((0.1 * pawTotal) + (1 * hayTotal) + (5 * flowerTotal) + (25 * bunnyTotal))
     const onedp = perSecond.toFixed(1)
     console.log(onedp)
     document.getElementById('idle-carrots').innerHTML = `${onedp} carrots per second`;
@@ -53,7 +59,7 @@ function pawBuy() {
     idleCarrots();
 }
 
-// Store item - PAW
+// Store item - HAY
 
 function hayBuy() {
     // Check to see if the player has enough carrots
@@ -69,6 +75,27 @@ function hayBuy() {
     // Increase cost each time user buys 1
     hayCost *= 1.15;
     document.getElementById('hay-cost').innerHTML = hayCost.toFixed(1);
+
+    // Invoke idleCarrots to increase carrots per second
+    idleCarrots();
+}
+
+// Store item - FLOWER
+
+function flowerBuy() {
+    // Check to see if the player has enough carrots
+    if (carrots < flowerCost) {
+        alert('Not enough carrots!')
+        return;
+    }
+    flowerTotal += 1;
+    carrots -= flowerCost;
+    document.getElementById('flower-total').innerHTML = `x${flowerTotal}`;
+    document.getElementById('total-carrots').innerHTML = Math.floor(carrots);
+
+    // Increase cost each time user buys 1
+    flowerCost *= 1.15;
+    document.getElementById('flower-cost').innerHTML = flowerCost.toFixed(1);
 
     // Invoke idleCarrots to increase carrots per second
     idleCarrots();
@@ -96,19 +123,42 @@ function bunnyBuy() {
 
 
 
+
+// Purchased upgrades array
+let purchasedUpgrades = [];
+
+
+var upgradeInterval = setInterval(checkUpgrades, 5000);
+if (allTimeCarrots < 100) {
+    const elements = document.getElementsByClassName('click-upgrade');
+    for (let element in elements) {
+        elements[element].hidden = true;
+    }
+}
+function checkUpgrades() {
+    if (allTimeCarrots >= 100 && (!purchasedUpgrades.includes('clickUpgrade'))) {
+        const elements = document.getElementsByClassName('click-upgrade');
+        for (let element in elements) {
+            elements[element].hidden = false;
+    }} 
+}
+
+
 function clickUpgrade() {
     if (carrots < 500) {
         alert('Not enough carrots!');
         return;
     }
     perClick = 2;
+    purchasedUpgrades.push('clickUpgrade')
 
     carrots -= 500;
     document.getElementById('total-carrots').innerHTML = Math.floor(carrots);
 
-    const elements = document.getElementsByClassName('click-upgrade');
-    for (let element in elements) {
-        elements[element].hidden = true;
-    }
-    // FIX THIS ^^
+    document.getElementsByClassName('click-upgrade')[1].hidden = true;
+    document.getElementsByClassName('click-upgrade')[0].disabled = true;
+    document.getElementsByClassName('click-upgrade')[0].style.borderColor = 'lime';
+    document.getElementsByClassName('click-upgrade')[0].style.setProperty('background-color', 'black');
+
+
 }
